c8df420401c5ab3762aa9feb90ab92d862f87c05
albabarrera <58370877+albabarrera@users.noreply.github.com>
2022-09-29 21:52:59 +0200

    Commit segundo ejercicio

---
+++ sesion-01-02-ejercicio-sintaxis-variables/info-personal.js
@@ -0,0 +1,13 @@
+const infoPersonal = [
+    "alba",
+    37,
+    false,
+    new Date('1985-05-31'),
+    {
+        title: "1984",
+        author: "George Orwell",
+        date: 1949,
+        url: "https://en.wikipedia.org/wiki/Nineteen_Eighty-Four"
+    }
+
+]
