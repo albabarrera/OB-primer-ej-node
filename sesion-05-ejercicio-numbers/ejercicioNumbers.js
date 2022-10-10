b64e2ab41e676ff3e5a79866c8db2ad2d8525014
albabarrera <58370877+albabarrera@users.noreply.github.com>
2022-10-10 15:02:22 +0200

    ejercicio numbers

---
+++ sesion-05-ejercicio-numbers/ejercicioNumbers.js
@@ -0,0 +1,6 @@
+let heightCentimeters = 158;
+let heightMeters = 1.58;
+let weightKilograms = 45.33;
+let roundedHeight = heightMeters.toFixed(1);
+let roundedWeight = Math.floor(weightKilograms);
+let max_valor_JS = (Number.MAX_VALUE + 1) === Number.MAX_VALUE
