d81e6f128dd018ed2e4253e16c69ee165cb99f5e
albabarrera <58370877+albabarrera@users.noreply.github.com>
2022-11-17 21:18:51 +0100

    sesion-13 ejercicio eslint

---
+++ sesion-13-eslint/ejercicio-sesion-04-textos.js
@@ -0,0 +1,22 @@
+let nombre = "Alba"
+let apellido = "Barrera"
+let segundoApellido = "Jimenez"
+
+let estudiante = `${nombre} ${apellido}`
+
+let estudianteMayus = estudiante.toUpperCase()
+
+let estudianteMinus = estudiante.toLowerCase()
+
+let estudianteLenght = estudiante.length
+
+let nombreFirstLetter = nombre[0]
+
+//Variable que contenga la última letra del Apellido
+let apellidoLength = apellido.length
+let apellidoLastLetter = apellido.charAt(apellidoLength - 1)
+//
+
+let estudianteNoSpaces = estudiante.replace(/ /g, "")
+
+let nombreInEstudiante = estudiante.includes(nombre)
